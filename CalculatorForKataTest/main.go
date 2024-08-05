7f1230028aa25a8ef3873b649d39a2312f5410b7
Liseykin.A.V. <yagasukakot@gmail.com>
2024-08-05 15:11:53 +0300

    Update main.go
    0.81 spelling fix

--- CalculatorForKataTest/main.go
+++ CalculatorForKataTest/main.go
@@ -24,7 +24,7 @@ func execute(a, b int, operator string) (rez int, err error) {
 	}
 }
 
-func romanToInt(str string) (rez int, err error) {
+func romanToInt(str string) (num int, err error) {
 
 	var roman = map[string]int{"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10}
 
@@ -36,7 +36,7 @@ func romanToInt(str string) (rez int, err error) {
 	return 0, fmt.Errorf("неверный ввод римских цифр: %s", str)
 }
 
-func intToRoman(num int) (rom string) {
+func intToRoman(num int) (roman string) {
 
 	val := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
 	sym := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
@@ -44,24 +44,23 @@ func intToRoman(num int) (rom string) {
 	for i := 0; i < len(val); i++ {
 		for num >= val[i] {
 			num -= val[i]
-			rom += sym[i]
+			roman += sym[i]
 		}
 	}
-	return rom
+	return roman
 }
 
-func stringToInt(str string) (rez int, err error) {
+func stringToInt(str string) (result int, err error) {
 
 	for i := 1; i <= 10; i++ {
 		if str == strconv.Itoa(i) {
-			rez = i
-			return rez, nil
+			return i, nil
 		}
 	}
 	return 0, fmt.Errorf("неверный ввод арабских цифр: %s", str)
 }
 
-func calculate(input string) (rezult string, err error) {
+func calculate(input string) (result string, err error) {
 
 	isArabic := strings.ContainsAny(input, "0123456789")
 	isRoman := strings.ContainsAny(input, "IVX")
@@ -121,10 +120,10 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	text, _ := reader.ReadString('\n')
 
-	rezult, err := calculate(text)
+	result, err := calculate(text)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Результат : ", rezult)
+	fmt.Println("Результат : ", result)
 }
